790c856e8ae9b9a7a71312f59eaf295c47dc2eaa
nicztheone99 <nicztheone99@gmail.com>
2022-04-04 11:27:00 +0700

    Upload to github

---
+++ src/main/java/com/maybank/springboot/library/MaybankSpringbootLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.maybank.springboot.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybankSpringbootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybankSpringbootLibraryApplication.class, args);
+	}
+
+}
